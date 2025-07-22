2a8052be954e406842781b2936d9f56fe8797cd6
AnnaPaytakov <paytakov.annamurad2003@gmail.com>
2025-07-22 18:38:26 +0500

    added api/users/auth/

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, ProfileViewSet
 from .views import RegisterUserView, MeView
+from rest_framework_simplejwt.views import TokenObtainPairView
 from django.urls import path
 
 
@@ -12,4 +13,5 @@ router.register(r'users', ProfileViewSet, basename='profile')
 urlpatterns = [
     path('users/register/', RegisterUserView.as_view(), name='user-register'),
     path('users/me/', MeView.as_view(), name='user-me'),
+    path('users/auth/', TokenObtainPairView.as_view(), name='token_obtain')
 ]
